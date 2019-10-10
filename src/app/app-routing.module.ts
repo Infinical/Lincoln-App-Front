34d7f445fd8e73d307dcd0eca1396ff2e62fdbfa
Infinical <inficalvin@gmail.com>
2019-10-10 23:50:07 +0300

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomepageComponent } from "./pages/homepage/homepage.component";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [{ path: "", component: HomepageComponent }];
+const routes: Routes = [{ path: "", component: HomepageComponent },
+{path: 'admin', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
